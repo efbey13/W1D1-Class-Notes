df17f3c796d236e5b437bae11f40b4faf6ecb540
efbey13 <efbey13@gmail.com>
2020-10-13 16:48:48 +0000

    Initial commit

---
+++ main.rb
@@ -0,0 +1,17 @@
+# Week 1 Day 1: Data Types
+# Pieces of infrmation we will be using
+
+# Numbers- Integers or Decimal Numbers
+# Strings- Words, any amount of characters, always surrounded by quotes
+# Booleans- Either true or false, never both at the same time
+
+# remainder will never be greater than the number on the right side of modulo operator
+# it will keep cycling from 0 up to but not including the number
+# ruby operators has its own order of operations
+# Negative indexing
+# neagative indexing starts at -1, it always gets the last character in the string no matter what
+puts 'Hello'[-1]
+puts 'Hello'[-2]
+puts 'Hello'[-3]
+puts 'Hello'[-4]
+puts 'Hello'[-5]
